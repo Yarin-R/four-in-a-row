3365ac00f67b07d139b59eb25c41d132c5e076fb
RainbowFireBeast <1SuperElmo1@gmail.com>
2019-01-11 17:44:43 +0200

    Graphics client working poc of board with canvas, control of each circle

--- client/Graphics.py
+++ client/Graphics.py
@@ -11,6 +11,7 @@ class Window(Frame):
         Frame.__init__(self, master)
         self.master = master
         self.init_window()
+        self.image_refs = []
 
     def init_window(self):
         self.master.title("Four in a Row")
@@ -18,7 +19,52 @@ class Window(Frame):
     def create_board(self):
 
         path = '/home/yarin/Desktop/four-in-a-row/client/imgs'
+        c = Canvas(self.master, width=640, height=480)
+        c.grid()
 
+
+
+        # player1
+        im = Image.open(os.path.join(path, 'player1_rect.png'))
+        resized = im.resize((90, 76), Image.ANTIALIAS)
+        p1_rect = ImageTk.PhotoImage(resized)
+        self.image_refs.append(p1_rect)
+
+        im = Image.open(os.path.join(path, 'player2_rect.png'))
+        resized = im.resize((90, 76), Image.ANTIALIAS)
+        p2_rect = ImageTk.PhotoImage(resized)
+        self.image_refs.append(p2_rect)
+
+        im = Image.open(os.path.join(path, 'player0_rect.png'))
+        resized = im.resize((90, 76), Image.ANTIALIAS)
+        p0_rect = ImageTk.PhotoImage(resized)
+        self.image_refs.append(p0_rect)
+
+        #c.create_image(0, 0, image=tkimage, anchor='nw')
+        b = True
+        # loop
+        for x in xrange(0, (640 / 7) * 6, 90):
+            b = not b
+            for y in xrange(0, (457 / 7) * 6, 76):
+                if b:
+                    c.create_image(x, y, image=p1_rect, anchor='nw')
+                else:
+                    c.create_image(x, y, image=p0_rect, anchor='nw')
+                b = not b
+        #"""
+        # background
+        im = Image.open(os.path.join(path, 'bg.png'))
+        resized = im.resize((640, 457), Image.ANTIALIAS)
+        tkimage = ImageTk.PhotoImage(resized)
+        self.image_refs.append(tkimage)
+        c.create_image(0, 0, image=tkimage, anchor='nw')
+        #"""
+
+
+
+
+
+        """
         # background
         im = Image.open(os.path.join(path, 'bg.png'))
         resized = im.resize((450, 450), Image.ANTIALIAS)
@@ -27,7 +73,7 @@ class Window(Frame):
         l.image = tkimage
         l.configure(image=l.image)
         l.place(x=0, y=0)
-
+        
         im = Image.open(os.path.join(path, 'player1.png'))
         resized = im.resize((64, 64), Image.ANTIALIAS)
         tkimage = ImageTk.PhotoImage(resized)
@@ -44,6 +90,7 @@ class Window(Frame):
             board.append(line)
 
         return board
+        """
 
 
 
@@ -52,7 +99,7 @@ class Window(Frame):
 
 
 root = Tk()
-root.geometry("640x480")
+root.geometry("650x480")
 root.resizable(0, 0)
 app = Window(root)
 
